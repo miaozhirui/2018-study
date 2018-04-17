6c0ba04837ad2d3974af8c72c4e0e9705d543d17
Dark <dark.miao@credan.com>
2018-04-17 16:36:10 +0800

    修复bug

--- mzr/libs/utils.js
+++ mzr/libs/utils.js
@@ -23,7 +23,7 @@ module.exports = {
 
         return {
 
-            noncestr: noncestr,
+            nonceStr: noncestr,
             timestamp: timestamp,
             signature: signature
         }
